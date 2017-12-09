9c7bbf9f57a5397fbfd28f69a37a5f39a049e505
jc_adavydenko <zabell2814@gmail.com>
2017-12-09 15:57:48 +0200

    styled header

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -3,13 +3,17 @@
 // You can remove or add your own function in this file.
 
 var common = {
-  headerSlider: function() {
-    var $slEl = $('.freq-slider');
-    if (!$slEl.length) {
-      return false;
+    headerSlider: function() {
+        var $slEl = $('.program-slider');
+        if (!$slEl.length) {
+            return false;
+        }
+        $slEl.owlCarousel({
+            items: 4,
+            dots: false,
+            nav: true
+        });
     }
-    $slEl.owlCarousel();
-  }
 };
 
 export default common;
